668ff7535ea060711664ae371d1b0237642e1db3
MikoCodes92 <mikiasgsilasie1920@gmail.com>
2026-01-07 21:43:03 +0300

    feat(gui): added a codebehind for event handling using navigation service

--- CManager.Presentation.GuiApp/MainWindow.xaml.cs
+++ CManager.Presentation.GuiApp/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CManager.Application.Services;
+using CManager.Infrastructure.Data;
+using CManager.Presentation.GuiApp.Services;
+using CManager.Presentation.GuiApp.ViewModels;
 
 namespace CManager.Presentation.GuiApp
 {
@@ -16,9 +13,54 @@ namespace CManager.Presentation.GuiApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NavigationService _navigationService;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeNavigation(); 
+        }
+
+        private void InitializeNavigation()
+        {
+            //set up data file path
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var appDirectory = Path.Combine(appDataPath, "CManager");   
+            var filePath = Path.Combine(appDirectory, "customers.json");
+
+            Directory.CreateDirectory(appDirectory);
+
+            //Create respository and service
+            var repository = new CustomerRepository(filePath);
+            var customerService = new CustomerService(repository);
+
+            //first let's Create navigation service 
+            _navigationService = new NavigationService(null!, null!); //we will set these after
+           
+            //create view models with navigation service
+            var customerListViewModel = new CustomerListViewModel(customerService, _navigationService);
+            var customerDetailViewModel = new CustomerDetailViewModel(customerService, _navigationService);
+
+            //update navigation service with view models
+            typeof(NavigationService)
+                .GetField("_customerListViewModel", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                ?.SetValue(_navigationService, customerListViewModel);
+
+            typeof(NavigationService)
+               .GetField("_customerDetailViewModel", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+               ?.SetValue(_navigationService, customerDetailViewModel);
+
+            //set content control and navigate to customer list
+            _navigationService.SetContentControl(MainContent);
+        }
+
+        private void CustomersButton_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.NavigateToCustomerList();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            _navigationService.NavigateToCreateCustomer();
         }
     }
 }
